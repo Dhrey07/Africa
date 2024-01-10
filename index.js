99b623bc64af9be8243d0fcd5cbf8786de8cb373
Damilare Joseph Oyebamiji <datamayor75@gmail.com>
2024-01-10 09:22:16 +0100

    final for inirial push

---
+++ index.js
@@ -0,0 +1,7 @@
+function subscribe() {
+    document.getElementById("sign").innerHTML = "Coming Soon"
+    transition: 1s;
+}
+function Signin() {
+    document.getElementById("sign").innerHTML = "Sign In"
+}
