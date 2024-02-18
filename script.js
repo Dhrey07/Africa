a42160c5d98538e838603f09f07e2d582f594a69
Damilare Joseph Oyebamiji <datamayor75@gmail.com>
2024-02-18 13:47:44 +0100

    Webinar updated

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function SendMail() {
         phone_number : document.getElementById("phone_number").value,
         email_id : document.getElementById("email_id").value,
         nationality : document.getElementById("nationality").value,
-        answer : document.getElementById("answer").value,
-        message_1 : document.getElementById("message_1").value,
-        message_2 : document.getElementById("message_2").value,
-        message_3 : document.getElementById("message_3").value,
-        message_4 : document.getElementById("message_4").value,
+        // answer : document.getElementById("answer").value,
+        // message_1 : document.getElementById("message_1").value,
+        // message_2 : document.getElementById("message_2").value,
+        // message_3 : document.getElementById("message_3").value,
+        // message_4 : document.getElementById("message_4").value,
     }
     
     emailjs.send("service_hmtfx5p","template_hs2savc",params).then(alert("Email Sent!!"))
